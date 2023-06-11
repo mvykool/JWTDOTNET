13aa02341006101d3aa372ed1e300869d4268c21
maicol hernandez <maykolandres4@gmail.com>
2023-06-11 00:54:45 -0500

    jjwt

---
+++ JWTDOTNET/Controllers/AuthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using JWTDOTNET.Models;
+
+namespace JWTDOTNET.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : Controller
+    {
+        public static User user = new User();
+    }
+}
