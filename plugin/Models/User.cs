bacfd5656867f6580d3a52a1845a19192cf12b68
Maicol Hernandez <maykolandres4@gmail.com>
2024-12-26 08:01:56 -0500

    deleted old files

--- plugin/Models/User.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace JWTDOTNET.Models
-{
-    public class User
-    {
-        public string Username { get; set; } = string.Empty;
-
-        public string PasswordHash { get; set; } = string.Empty;
-    }
-}
